3f6bc4abd7403060e8e4d9ae4f72f1399c4db8b6
Israa Odeh <Israa.K.Odeh@hotmail.com>
2023-10-21 16:05:37 +0300

    Modify the main file to set the "TopOuterScreen" as the new home page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:touristine/LoginAndRegistration/MainPages/landingPage.dart';
+import 'package:touristine/LoginAndRegistration/MainPages/TopOuterScreen.dart';
 
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LandingPage(),
+      home: TopOuterScreen(),
     );
 }
 }
