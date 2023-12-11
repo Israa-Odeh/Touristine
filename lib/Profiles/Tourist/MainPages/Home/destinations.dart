9f4c302194b68431dd6bca2bf7ab7ba7b4c69d83
Israa Odeh <Israa.K.Odeh@hotmail.com>
2023-12-11 04:44:27 +0200

    Adding a progress bar indicator to the clicked destination.

--- lib/Profiles/Tourist/MainPages/Home/destinations.dart
+++ lib/Profiles/Tourist/MainPages/Home/destinations.dart
@@ -11,11 +11,12 @@ class DestinationList extends StatefulWidget {
   final String listTitle;
   final String token;
 
-  const DestinationList(
-      {super.key,
-      required this.destinations,
-      required this.listTitle,
-      required this.token});
+  const DestinationList({
+    super.key,
+    required this.destinations,
+    required this.listTitle,
+    required this.token,
+  });
 
   @override
   _DestinationListState createState() => _DestinationListState();
@@ -26,6 +27,7 @@ class _DestinationListState extends State<DestinationList> {
   int _selectedTileIndex = -1;
   int _currentPageIndex = 0;
   late Timer _timer;
+  bool _isLoading = false;
 
   Map<String, dynamic> destinationDetails = {};
   List<Map<String, dynamic>> destinationImages = [];
@@ -33,10 +35,13 @@ class _DestinationListState extends State<DestinationList> {
 
   // A function to retrieve all of the destination details.
   Future<void> getDestinationDetails(String destName) async {
-    final url =
-        Uri.parse('https://touristine.onrender.com/get-destination-details');
+    final url = Uri.parse('https://touristine.onrender.com/get-destination-details');
 
     try {
+      setState(() {
+        _isLoading = true;
+      });
+
       final response = await http.post(
         url,
         headers: {
@@ -48,14 +53,17 @@ class _DestinationListState extends State<DestinationList> {
         },
       );
 
+      setState(() {
+        _isLoading = false;
+      });
+
       if (response.statusCode == 200) {
         // Success.
         // Parse the response body.
         final Map<String, dynamic> responseData = json.decode(response.body);
 
         // Extract destinationImages as List<Map<String, dynamic>>
-        destinationImages =
-            List<Map<String, dynamic>>.from(responseData['destinationImages']);
+        destinationImages = List<Map<String, dynamic>>.from(responseData['destinationImages']);
 
         // Access destination details and other data.
         destinationDetails = responseData['destinationDetails'];
@@ -68,11 +76,9 @@ class _DestinationListState extends State<DestinationList> {
       } else if (response.statusCode == 500) {
         final Map<String, dynamic> responseData = json.decode(response.body);
         if (responseData.containsKey('error')) {
-          if (responseData['error'] ==
-              'Details for this destination are not available') {
+          if (responseData['error'] == 'Details for this destination are not available') {
             // ignore: use_build_context_synchronously
-            showCustomSnackBar(context, 'Place details aren\'t available',
-                bottomMargin: 0);
+            showCustomSnackBar(context, 'Place details aren\'t available', bottomMargin: 0);
           } else {
             // ignore: use_build_context_synchronously
             showCustomSnackBar(context, responseData['error'], bottomMargin: 0);
@@ -80,8 +86,7 @@ class _DestinationListState extends State<DestinationList> {
         }
       } else {
         // ignore: use_build_context_synchronously
-        showCustomSnackBar(context, 'Error retrieving place details',
-            bottomMargin: 0);
+        showCustomSnackBar(context, 'Error retrieving place details', bottomMargin: 0);
       }
     } catch (error) {
       print('Failed to fetch place details: $error');
@@ -171,67 +176,76 @@ class _DestinationListState extends State<DestinationList> {
             child: SizedBox(
               height: 230,
               width: 400,
-              child: PageView.builder(
-                controller: _pageController,
-                itemCount: widget.destinations.length,
-                onPageChanged: (int index) {
-                  setState(() {
-                    _currentPageIndex = index;
-                  });
-                },
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onTap: () {
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  PageView.builder(
+                    controller: _pageController,
+                    itemCount: widget.destinations.length,
+                    onPageChanged: (int index) {
                       setState(() {
-                        _selectedTileIndex = index;
+                        _currentPageIndex = index;
                       });
-                      navigateToDetailsPage(index);
-                      print('Clicked on ${widget.destinations[index]['name']}');
                     },
-                    child: Container(
-                      margin: const EdgeInsets.all(0),
-                      child: Card(
-                        color: _selectedTileIndex == index
-                            ? const Color(0xFF1E889E)
-                            : const Color.fromARGB(255, 255, 255, 255),
-                        shape: RoundedRectangleBorder(
-                          side: const BorderSide(
-                              color: Color(0xFF1E889E), width: 3),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                          children: [
-                            Image.network(
-                              widget.destinations[index]['image'],
-                              width: 400,
-                              height: 165,
-                              fit: BoxFit.fill,
+                    itemBuilder: (BuildContext context, int index) {
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            _selectedTileIndex = index;
+                          });
+                          navigateToDetailsPage(index);
+                          print('Clicked on ${widget.destinations[index]['name']}');
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(0),
+                          child: Card(
+                            color: _selectedTileIndex == index
+                                ? const Color.fromARGB(255, 231, 231, 231)
+                                : const Color.fromARGB(255, 255, 255, 255),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                  color: Color(0xFF1E889E), width: 3),
+                              borderRadius: BorderRadius.circular(20),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: FittedBox(
-                                fit: BoxFit.scaleDown,
-                                child: Text(
-                                  widget.destinations[index]['name'],
-                                  style: TextStyle(
-                                    color: _selectedTileIndex == index
-                                        ? Colors.white
-                                        : const Color(0xFF1E889E),
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Zilla',
-                                  ),
-                                  maxLines: 1,
+                            clipBehavior: Clip.antiAlias,
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  widget.destinations[index]['image'],
+                                  width: 400,
+                                  height: 165,
+                                  fit: BoxFit.fill,
                                 ),
-                              ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: FittedBox(
+                                    fit: BoxFit.scaleDown,
+                                    child: Text(
+                                      widget.destinations[index]['name'],
+                                      style: TextStyle(
+                                        color: _selectedTileIndex == index
+                                            ? const Color.fromARGB(255, 25, 114, 132)
+                                            : const Color(0xFF1E889E),
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Zilla',
+                                      ),
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                },
+                      );
+                    },
+                  ),
+                  if (_isLoading) const CircularProgressIndicator(
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Color(0xFF1E889E)),
+                  ),
+                ],
               ),
             ),
           ),
