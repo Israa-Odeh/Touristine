877551709bebb6b5abf9dbb03bad7348015fc8f0
Israa Odeh <Israa.K.Odeh@hotmail.com>
2023-12-12 18:17:45 +0200

    Make the image fill the whole space.

--- lib/Profiles/Tourist/MainPages/Home/complaints.dart
+++ lib/Profiles/Tourist/MainPages/Home/complaints.dart
@@ -180,7 +180,7 @@ class ComplaintCard extends StatelessWidget {
                                     (complaint['images'] as List)[imgIndex],
                                     width: 100,
                                     height: 100,
-                                    fit: BoxFit.cover,
+                                    fit: BoxFit.fill,
                                   ),
                                 ),
                               ),
