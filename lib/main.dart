a344fba0038eb238698ed9eca76fb091e2bbd00c
Israa Odeh <Israa.K.Odeh@hotmail.com>
2024-01-03 18:57:14 +0200

    chatting test - tourist side.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:touristine/LoginAndRegistration/MainPages/TopOuterScreen.dart';
 import 'package:touristine/UserData/userProvider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
