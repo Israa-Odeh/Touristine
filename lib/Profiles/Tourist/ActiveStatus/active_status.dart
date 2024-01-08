9cad910a82600d57897679b9763694b3d23806b9
Israa Odeh <Israa.K.Odeh@hotmail.com>
2024-01-08 20:14:50 +0200

    active_status tourist.

--- lib/Profiles/Tourist/ActiveStatus/active_status.dart
+++ lib/Profiles/Tourist/ActiveStatus/active_status.dart
@@ -9,9 +9,31 @@ void setTouristActiveStatus(String touristEmail, bool isActive) {
     'lastUpdate': FieldValue.serverTimestamp(),
   }).catchError((_) {
     // If the document doesn't exist, it will be created.
-    FirebaseFirestore.instance.collection('tourists_active_status').doc(touristEmail).set({
+    FirebaseFirestore.instance
+        .collection('tourists_active_status')
+        .doc(touristEmail)
+        .set({
       'active': isActive,
       'lastUpdate': FieldValue.serverTimestamp(),
     });
   });
+}
+
+Future<bool?> getAdminActiveStatus(String adminEmail) async {
+  try {
+    DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
+        .instance
+        .collection('admins_active_status')
+        .doc(adminEmail)
+        .get();
+
+    if (snapshot.exists) {
+      return snapshot.data()?['active'] as bool?;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    print('admins_active_status: $e');
+    return null;
+  }
 }
