4290bb20dcd865a657b94fd9e3833e554107bf7f
Israa Odeh <Israa.K.Odeh@hotmail.com>
2024-01-10 01:19:00 +0200

    Add firebase_initilaizer for the web.

---
+++ lib/WebApplication/Launcher/firebase_initializer.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> initializeFirebaseWeb() async {
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDzAHOWKiZDM97eOAjq4SuECDOFAPZ2YHs",
+          appId: "1:889464890314:web:26147ed3501be78f06e533",
+          messagingSenderId: "889464890314",
+          projectId: "touristine-authentication"));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:touristine/AndroidMobileApp/Launcher/launcher.dart';
+import 'package:touristine/WebApplication/Launcher/launcher.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -5,16 +7,9 @@ import 'package:flutter/material.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyDzAHOWKiZDM97eOAjq4SuECDOFAPZ2YHs",
-            appId: "1:889464890314:web:26147ed3501be78f06e533",
-            messagingSenderId: "889464890314",
-            projectId: "touristine-authentication"));
+    await initializeFirebaseWeb();
   }
   await Firebase.initializeApp();
 
-  // runApp(const MobileAppLauncher());
+  kIsWeb ? runApp(const WebAppLauncher()) : runApp(const MobileAppLauncher());
 }
-
-
