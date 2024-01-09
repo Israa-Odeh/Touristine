6bafb0374ea1b1f169a53eeb4d4a74d4f91c16f6
Israa Odeh <Israa.K.Odeh@hotmail.com>
2024-01-10 01:29:53 +0200

    Update the main file.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   if (kIsWeb) {
     await initializeFirebaseWeb();
   } else {
