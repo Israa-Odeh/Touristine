b9608910e52c1a91309d5a9502ddeba6242720a6
Israa Odeh <Israa.K.Odeh@hotmail.com>
2024-01-09 19:19:35 +0200

    Firebase.initializeApp() with web.

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,14 @@ import 'package:flutter/material.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDzAHOWKiZDM97eOAjq4SuECDOFAPZ2YHs",
+            appId: "1:889464890314:web:26147ed3501be78f06e533",
+            messagingSenderId: "889464890314",
+            projectId: "touristine-authentication"));
   }
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyDzAHOWKiZDM97eOAjq4SuECDOFAPZ2YHs",
-          appId: "1:889464890314:web:26147ed3501be78f06e533",
-          messagingSenderId: "889464890314",
-          projectId: "touristine-authentication"));
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
